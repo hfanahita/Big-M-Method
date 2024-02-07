4877a152f1fe039c8e52f8a9d0cb4cfa4864d120
hfanahita <anahita.hedayatifard@gmail.com>
2024-02-07 14:01:54 +0330

    Test simplex for an unbounded LP.

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ import matplotlib
 # input : c: one dimensional array, A: m*n matrix, b: one dimensional array
 
 # n = int(input("Please enter the number of variables: "))
-n = 5
+n = 4
 # m = int(input("Please enter the number of the constructive constraints: "))
-m = 3
+m = 2
 # print("Please enter the n coefficients of the objective function: ")
 # c = np.array([])
 # for i in range(n):
 #     x = float(input())
 #     c = np.append(c,x)
-c = np.array([-2, -6, 0, 0, 0])
+c = np.array([0.5, -4, -3, 1.5])
 
 # b_input = input("Enter b").split()
 # b = np.array([[x] for x in b_input])
-b = np.array([[10],[3],[4]])
+b = np.array([[2],[4]])
 x = np.zeros(n)
 
 # print("Please enter the coefficient matrix A element by element:")
@@ -27,11 +27,11 @@ x = np.zeros(n)
 # for i in range(m):
 #     for j in range(n):
 #         A[i,j] = float(input())
-A = np.array([[2,5,1,0,0],[-3, 3, 0, 1, 0],[1,0,0,0,1]])
+A = np.array([[1,1,-1,0],[0, 2, -1, 1]])
 print(A)
 
-x = np.array([0.0,0.0,10.0,3.0,4.0])
-B = [[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]]
-j_N = [0,1]
-j_b = [2,3,4]
+x = np.array([2.0,0.0,0.0,4.0])
+B = [[1.0,0.0],[0.0,1.0]]
+j_N = [1,2]
+j_b = [0,3]
 print(simplex(m,n,c,A,b,B,x,j_N,j_b))
